0bc18abbff358d14e33618d1719b99bd3835b79b
Hayden <43462177+Theoogamar@users.noreply.github.com>
2020-07-31 03:34:26 +1000

    Merge remote-tracking branch 'upstream/master'

---
+++ YoutubeExplode.Tests/ClosedCaptionSpecs.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using YoutubeExplode.Tests.Fixtures;
+
+namespace YoutubeExplode.Tests
+{
+    public class ClosedCaptionSpecs : IClassFixture<TempOutputFixture>
+    {
+        private readonly TempOutputFixture _tempOutputFixture;
+
+        public ClosedCaptionSpecs(TempOutputFixture tempOutputFixture) =>
+            _tempOutputFixture = tempOutputFixture;
+
+        [Theory]
+        [InlineData("_QdPW8JrYzQ")]
+        [InlineData("YltHGKX80Y8")]
+        public async Task I_can_get_available_closed_caption_tracks_of_any_available_YouTube_video(string videoId)
+        {
+            // Arrange
+            var youtube = new YoutubeClient();
+
+            // Act
+            var manifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(videoId);
+
+            // Assert
+            manifest.Tracks.Should().NotBeEmpty();
+        }
+
+        [Theory]
+        [InlineData("_QdPW8JrYzQ")]
+        [InlineData("YltHGKX80Y8")]
+        public async Task I_can_get_a_specific_closed_caption_track_of_any_available_YouTube_video(string videoId)
+        {
+            // Arrange
+            var youtube = new YoutubeClient();
+
+            // Act
+            var manifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(videoId);
+            var trackInfo = manifest.Tracks.First();
+            var track = await youtube.Videos.ClosedCaptions.GetAsync(trackInfo);
+
+            // Assert
+            track.Captions.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task I_can_extract_a_closed_caption_that_appears_at_a_specific_time_on_an_available_YouTube_video()
+        {
+            // Arrange
+            const string videoUrl = "https://www.youtube.com/watch?v=YltHGKX80Y8";
+            var youtube = new YoutubeClient();
+
+            // Act
+            var manifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(videoUrl);
+            var trackInfo = manifest.TryGetByLanguage("en")!;
+            var track = await youtube.Videos.ClosedCaptions.GetAsync(trackInfo);
+            var caption = track.TryGetByTime(new TimeSpan(00, 10, 41))!;
+            var captionPart = caption.TryGetPartByTime(TimeSpan.FromSeconds(0.65))!;
+
+            // Assert
+            caption.Should().NotBeNull();
+            captionPart.Should().NotBeNull();
+            caption.Text.Should().Be("know I worked really hard on not doing");
+            captionPart.Text.Should().Be(" hard");
+        }
+
+        [Theory]
+        [InlineData("_QdPW8JrYzQ")]
+        [InlineData("YltHGKX80Y8")]
+        public async Task I_can_download_a_specific_closed_caption_track_of_any_available_YouTube_video(string videoId)
+        {
+            // Arrange
+            var filePath = _tempOutputFixture.GetTempFilePath();
+            var youtube = new YoutubeClient();
+
+            // Act
+            var manifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(videoId);
+            var trackInfo = manifest.Tracks.First();
+            await youtube.Videos.ClosedCaptions.DownloadAsync(trackInfo, filePath);
+
+            // Assert
+            File.Exists(filePath).Should().BeTrue();
+        }
+    }
+}
---
+++ YoutubeExplode.Tests/SearchSpecs.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace YoutubeExplode.Tests
+{
+    public class SearchSpecs
+    {
+        [Fact]
+        public async Task I_can_search_for_YouTube_videos()
+        {
+            // Arrange
+            var youtube = new YoutubeClient();
+
+            // Act
+            var videos = await youtube.Search.GetVideosAsync("undead corporation megalomania");
+
+            // Assert
+            videos.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task I_can_search_for_YouTube_videos_and_get_a_subset_of_results()
+        {
+            // Arrange
+            const int maxVideoCount = 50;
+            var youtube = new YoutubeClient();
+
+            // Act
+            var videos = await youtube.Search.GetVideosAsync("billie eilish").BufferAsync(maxVideoCount);
+
+            // Assert
+            videos.Should().NotBeEmpty();
+            videos.Should().HaveCountLessOrEqualTo(maxVideoCount);
+        }
+
+        [Fact]
+        public async Task I_can_search_for_YouTube_videos_and_get_a_subset_of_results_from_a_specific_page()
+        {
+            // Arrange
+            var youtube = new YoutubeClient();
+
+            // Act
+            var videos = await youtube.Search.GetVideosAsync("billie eilish", 2, 1);
+
+            // Assert
+            videos.Should().NotBeEmpty();
+            videos.Should().HaveCountLessOrEqualTo(30);
+        }
+    }
+}
